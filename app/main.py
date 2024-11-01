dd439ad05fa17cc1d3ca29ef23b6a4b915ef078f
jt151077 <jt151077@gmail.com>
2024-11-01 13:36:13 +0000

    api service

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, jsonify, request
-import json, requests
-
-
 app = Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
     if request.method=='GET':
-        return jsonify({'Success':"You have hit the backend"})  
-
+        return jsonify({"message": "Api served from a Flask app in Cloud Run"})
     else:
         return jsonify({'Error':"This is a GET API method"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
